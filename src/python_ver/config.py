a09eb2a001585cd6963f8c4d8aa42e26d20a0888
EmperorAkashi <linchen4869@ucla.edu>
2023-07-15 12:18:35 -0700

    start from config and 1d langevin

---
+++ src/python_ver/config.py
@@ -0,0 +1,21 @@
+import dataclasses
+from typing import Optional, Tuple, List
+import omegaconf
+
+@dataclasses.dataclass
+class Langevin1DConfig:
+    Nsteps:int = 50000    
+    dt:float = 0.01                              
+    U0:float = 3.0                         
+    Sigma:float = 3.0
+    gamma:float = 1.5
+    k:float = 0.017          #spring for the naived harmonic potential
+    R:float = 1.0
+    R1:float = 0.08
+    R0:float = 0.8
+    alpha:float = 5.0
+    area:float = 0.3,
+    epsilon:float = 0.00
+    omg:float = omegaconf.MISSING 
+    A:float =  omegaconf.MISSING
+    relx_t:int = 100
