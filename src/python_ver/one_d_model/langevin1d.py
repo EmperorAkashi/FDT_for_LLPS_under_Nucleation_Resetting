2b78fd4476e234a28151fcd2a8a19f2faf4780ee
EmperorAkashi <linchen4869@ucla.edu>
2024-05-17 14:10:49 -0700

    fix R thre

--- src/python_ver/one_d_model/langevin1d.py
+++ src/python_ver/one_d_model/langevin1d.py
@@ -6,7 +6,7 @@ import python_ver.one_d_model.eom_util as E
 def langevin_1d(config:cf.Langevin1DConfig) -> None:
     r = 0.0 # 1d displacement starts from origin by default
     step = 0
-    R_curr = config.R_init # init R_ as R
+    R_curr = config.R_init # init R_curr as R
     nucleation = E.PoissonTime(config.tau)
     disp_path = config.disp_path
     radi_path = config.radi_path
@@ -14,7 +14,7 @@ def langevin_1d(config:cf.Langevin1DConfig) -> None:
     file_disp = open(disp_path, 'a')
     file_radi = open(radi_path, 'a')
 
-    while step < config.Nsteps and R_curr > config.R_thre:
+    while step < config.Nsteps and R_curr >= config.R_thre:
         step += 1
         r = E.trans_r(config.dt, r, config.alpha, config.c, 
                       R_curr, step,
