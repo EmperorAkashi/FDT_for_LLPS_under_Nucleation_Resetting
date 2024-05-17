326f599869e0aadab586513f17badbaf0fb9ac77
EmperorAkashi <linchen4869@ucla.edu>
2024-05-17 12:49:29 -0700

    wrap berne into a fn

--- src/python_ver/one_d_model/inverse_cdf.py
+++ src/python_ver/one_d_model/inverse_cdf.py
@@ -15,15 +15,17 @@ def CDF_Berne_FPT(x_val:float,D:float, x_0:float, k:float) -> List:
     result, _ = quad(Berne_FPT, 0, x_val, args=(D, x_0, k))
     return result
 
-x_values = np.linspace(0, 35, 1000)
-cdf_values = [CDF_Berne_FPT(x_val) for x_val in x_values]
-
-# Create an interpolation of the inverse CDF
-inverse_cdf = interp1d(cdf_values, x_values, fill_value="extrapolate")
+def get_berne_cdf():
+    x_values = np.linspace(0, 35, 1000)
+    cdf_values = [CDF_Berne_FPT(x_val) for x_val in x_values]
+    # Create an interpolation of the inverse CDF
+    inverse_cdf = interp1d(cdf_values, x_values, fill_value="extrapolate")
+    return inverse_cdf
 
 
 def sample_from_Berne_FPT():
     # Generate uniform random samples
     u = np.random.rand()
     # Transform them using the inverse CDF
-    return inverse_cdf(u)
+    inv_cdf = get_berne_cdf()
+    return inv_cdf(u)
--- src/python_ver/one_d_model/test_module.py
+++
@@ -1,3 +0,0 @@
-import python_ver.one_d_model.main_langevin
-
-print("import succesfully")
